95196352153c92c1397fca2dbdc757ac5a41dd53
Victor Velozo <victor.velozo@outlook.com>
2023-09-15 15:21:22 -0300

    chore: fastify, eslint, knex setup

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+import { knex } from './database'
+
+const server = fastify()
+
+server.get('/', async () => {
+  const test = await knex('sqlite_schema').select('*')
+  return test
+})
+
+server.listen({ port: 3333 }).then(() => {
+  console.log('Server running in http://localhost:3333')
+})
